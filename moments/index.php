64c53193e296d839acc46b02919a80f95c9daf91
Nicolas Solerieu <nsolerieu@github.com>
2022-12-29 11:48:20 -0800

    Full prod pull

--- moments/index.php
+++ moments/index.php
@@ -75,22 +75,27 @@
       <div class="row">
         <div class="col-50">
           <p class="text-regular">
-            What is this?
+            <b>What is this?</b>
           </p>
         </div>
         <div class="col-50">
           <p class="text-regular">
-            I got away from instagram but still wanted a place to have have something similar.
-            This is a sort of of visual timeline that just requires me to drop a photo in folder.
-            Although this is an attempt at exiting the voyeurism of sociual media, there is still some ego in needing to keep a record. I'm working on it, here.
+            I got away from instagram but still wanted a place to keep my photos.
+            <br><br>
             This is mostly intended for my personal consumption and will live on my domain. If you read these words and feel anything from perusing this gallery, that's great. But there is really no goal to this.
-            We all see things. Life happens all around us. Few can catch and hold on to the feeling of a moment withou the help of photography. These are intentionally low-res, the moment matter. I remember the details.
+            We all see things. Life happens all around us. Few can catch and hold on to the feeling of a moment without the help of photography.
+            <br><br>
+            I'm building this thing because there doesn't seem to be much satisfiying alternative to instagram. Although this is an attempt at exiting the voyeurism of social media, there is still some ego in needing to keep a record. I'm working on it, here.
+            <br><br>
+            I'm trying to not grow my footprint too much and tame my ego. These compressed JPGs will hosted here as I'm purging all my cloud accounts and other devices.
+            <br><br>
+            It's simple like a folder, but online. A place to store all of my photos that are currently scattered accross google drive, my phone library, old phones, my instagram archive, and a bunch of SD cards. The images are intentionally low-res, the moment matter. I remember the details.
         </div>
       </div>
     </div>
   </footer>
 
-  <footer class="padded-section">
+  <footer>
     <div class="container">
       <div class="row full-row-border-top">
         <div class="col-100">
